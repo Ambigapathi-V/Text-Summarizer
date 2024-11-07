c98e1d82c70dbfabdec0707a267139125b0eb844
Ambigapath <168761904+Ambigapath@users.noreply.github.com>
2024-11-07 13:50:14 +0530

    aadded pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.TextSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +8,18 @@ try:
         logger.info(f".........Starting {STAGE_NAME}")
         data_ingestion_pipeline = DataIngestionTrainingPipeline()
         data_ingestion_pipeline.initiate_data_ingestion()
+        logger.info(f"{STAGE_NAME} completed successfully......")
+        
+except Exception as e:
+    logger.error(f"Error occurred in {STAGE_NAME}: {str(e)}")
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+        logger.info(f".........Starting {STAGE_NAME}")
+        data_ingestion_pipeline = DataTransformationTrainingPipeline()
+        data_ingestion_pipeline.initiate_data_transformation()
         logger.info(f"{STAGE_NAME} completed successfully......")
         
 except Exception as e:
---
+++ src/TextSummarizer/pipeline/data_transformation_pipeline.py
@@ -0,0 +1,18 @@
+from src.TextSummarizer.config.configuration import ConfigurationManager
+from src.TextSummarizer.components.data_transformation import DataTransformation
+from src.TextSummarizer.logging import logger
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def initiate_data_transformation(self):
+
+        # Load configuration
+        config = ConfigurationManager()
+
+        data_transformation_config = config.get_data_transformation_config()
+
+        data_transformation = DataTransformation(data_transformation_config)
+
+        data_transformation.convert()
